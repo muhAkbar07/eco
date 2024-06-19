0d17e0d0cbc322a3156495d859b3c61149846272
muhAkbar07 <muhakbar072x@gmail.com>
2024-06-19 14:23:51 +0700

    fitur homepage

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+#[Title('Product Detail - Ecommerce')]
 class ProductDetailPage extends Component
 {
-    public function render()
+
+    public $slug;
+
+    public function mount($slug)
     {
-        return view('livewire.product-detail-page');
+        $this->slug = $slug;
+    } 
+    public function render(){
+        
+        return view('livewire.product-detail-page', [
+            'product' => Product::where('slug', $this->slug)->firstOrFail()
+        ]);
     }
 }
