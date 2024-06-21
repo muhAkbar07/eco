7be8ab5d12385d8127182adf93b855c3a0c57ae2
muhAkbar07 <muhakbar072x@gmail.com>
2024-06-21 14:17:04 +0700

    chackout shoop

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -6,13 +6,18 @@ use App\Models\Brand;
 use App\Models\Product;
 use Livewire\Component;
 use App\Models\Category;
+use Livewire\Attributes\Url;
 use Livewire\WithPagination;
 use Livewire\Attributes\Title;
-use Livewire\Attributes\Url;
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 
 #[Title('Products - Ecommerce')]
 class ProductsPage extends Component{
+
+    use LivewireAlert;
 
     use WithPagination;
     #[Url]
@@ -25,28 +30,51 @@ class ProductsPage extends Component{
     public $on_sale = [];
     #[Url]
     public $price_range = 15000000;
+    #[Url]
+    public $sort = 'latest';
 
+    // add to cart
+    public function addToCart($product_id){
+        $total_count = CartManagement::addItemToCart($product_id);
+
+        $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
+
+        $this->alert('success', 'Product Added successfully!', [
+            'position' => 'bottom-end',
+            'timer' => 3000,
+            'toast' => true,
+           ]);
+    }
     
     public function render(){
         $productQuery = Product::query()->where('is_active', 1);
 
-        if(!empty($this->selected_categories)){
+        if (!empty($this->selected_categories)){
             $productQuery->whereIn('category_id', $this->selected_categories);
         }
-        if(!empty($this->selected_brands)){
+        if (!empty($this->selected_brands)){
             $productQuery->whereIn('brand_id', $this->selected_brands);
         }
 
-        if($this->featured){
+        if ($this->featured){
             $productQuery->where('is_featured', 1);
         }
 
-        if($this->on_sale){
+        if ($this->on_sale){
             $productQuery->where('on_sale', 1);
         }
 
-        if($this->price_range){
+        if ($this->price_range){
             $productQuery->whereBetween('price', [0, $this->price_range]);
+        }
+        
+       
+        if ($this->sort == 'latest') {
+            $productQuery->latest();
+        }
+
+        if ($this->sort == 'price'){
+            $productQuery->orderBy('price');
         }
 
         
